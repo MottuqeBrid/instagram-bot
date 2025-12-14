3de6786e787ec472d76c7ddc560f2f62fbff8063
Mottuqe Brid <mottuqebrid@gmail.com>
2025-12-14 15:07:14 +0600

    Add basic functionality for Instagram bot operations

---
+++ main.py
@@ -0,0 +1,10 @@
+from instabot import Bot
+
+bot = Bot()
+
+bot.login(username="", password="")
+
+bot.follow("mottuqebrid")
+
+bot.upload_photo("", caption="I love python")  # input path
+bot.unfollow("mottuqebrid")
